58d7a1e323c48b828d0a1e8e8226e8d47add5aa9
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-03-23 10:15:55 +0700

    learn event and method

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 const { createApp } = Vue;
+
 const desc = {
   title: "hello",
   description: "first vue app",
-  imgSrc: "https://source.unsplash.com/200x200/?nature",
+  // imgSrc: "https://source.unsplash.com/200x200/?nature",
 };
 
 const cardDetails = [
   {
     id: "1",
     title: "Natural",
-    imgUrl: "https://source.unsplash.com/200x200/?nature",
+    // imgUrl: "https://source.unsplash.com/200x200/?nature",
   },
   {
     id: "2",
     title: "astronomy",
-    imgUrl: "https://source.unsplash.com/200x200/?astronomy",
+    // imgUrl: "https://source.unsplash.com/200x200/?astronomy",
   },
   {
     id: "3",
     title: "programming",
-    imgUrl: "https://source.unsplash.com/200x200/?programming",
+    // imgUrl: "https://source.unsplash.com/200x200/?programming",
   },
   {
     id: "4",
     title: "design",
-    imgUrl: "https://source.unsplash.com/200x200/?design",
+    // imgUrl: "https://source.unsplash.com/200x200/?design",
   },
   {
     id: "5",
     title: "design",
-    imgUrl: "https://source.unsplash.com/200x200/?design",
+    // imgUrl: "https://source.unsplash.com/200x200/?design",
   },
   {
     id: "6",
     title: "design",
-    imgUrl: "https://source.unsplash.com/200x200/?design",
+    // imgUrl: "https://source.unsplash.com/200x200/?design",
   },
 ];
 
@@ -87,11 +88,43 @@ const getProducts = createApp({
       products: null,
     };
   },
-  mounted() {
-    fetch("https://dummyjson.com/products?limit=10")
-      .then((res) => res.json())
-      .then((res) => (this.products = res.products));
-  },
+  // mounted() {
+  //   fetch("https://dummyjson.com/products?limit=10")
+  //     .then((res) => res.json())
+  //     .then((res) => (this.products = res.products));
+  // },
 });
 
 getProducts.mount("#products");
+
+const addProducts = createApp({
+  data() {
+    return {
+      products: null,
+      limit: 1,
+      cart: [],
+    };
+  },
+  mounted() {
+    fetch(`https://dummyjson.com/products?limit=10`)
+      .then((res) => res.json())
+      .then((res) => (this.products = res.products));
+  },
+  // note: will learn this later...
+  // updated() {
+  //   fetch(`https://dummyjson.com/products?limit=${this.limit}`)
+  //     .then((res) => res.json())
+  //     .then((res) => (this.products = res.products));
+  // },
+  methods: {
+    moreProducts() {
+      this.limit += 1;
+    },
+    addToCart(data) {
+      this.cart.push(data);
+      console.log(this.cart);
+    },
+  },
+});
+
+addProducts.mount("#add-products");
