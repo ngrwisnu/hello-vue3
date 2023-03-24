9406af2e92b6b6e13325514e6724d6ded202a4ac
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-03-24 09:33:59 +0700

    chapter 2: computed property

--- scripts/chapter-two.js
+++ scripts/chapter-two.js
@@ -9,3 +9,30 @@ const parsedText = createApp({
 });
 
 parsedText.mount("#parsed-text");
+
+const computedProp = createApp({
+  data() {
+    return {
+      placeholder: "Slug for this page",
+      count: 1,
+    };
+  },
+  computed: {
+    computedTitle() {
+      return this.placeholder.toLowerCase().replace(/\s/g, "-");
+    },
+    counterFromComputed() {
+      return this.count === 1 ? 1 : this.count * 10;
+    },
+  },
+  methods: {
+    counterFromMethod() {
+      return +new Date();
+    },
+    increment() {
+      this.count += 1;
+    },
+  },
+});
+
+computedProp.mount("#computed-section");
